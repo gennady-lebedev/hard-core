41642705542736242236200d0a61260f4a7d84c2
gennady <gennady.lebedev@gmail.com>
2019-05-19 11:29:14 +0300

    add repository cases

--- example/src/test/scala/dev/rudiments/sample/domain/spec/GeneratorSpec.scala
+++ example/src/test/scala/dev/rudiments/sample/domain/spec/GeneratorSpec.scala
@@ -1,9 +1,10 @@
 package dev.rudiments.sample.domain.spec
 
 import dev.rudiments.hardcore.dsl._
+import dev.rudiments.hardcore.dsl.ID._
 import dev.rudiments.hardcore.repo.memory.MemoryRepo
 import dev.rudiments.sample.domain.DayOfWeek._
-import dev.rudiments.sample.domain.{GeneratedConfig, GeneratorConfig}
+import dev.rudiments.sample.domain.{GeneratedConfig, GeneratedValues, GeneratorConfig}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
@@ -11,17 +12,7 @@ import org.scalatest.Matchers._
 
 @RunWith(classOf[JUnitRunner])
 class GeneratorSpec extends WordSpec with Matchers {
-  implicit val configsMeta: Meta[GeneratorConfig] = Meta(value => ID(value.id))
-  val configs: MemoryRepo[GeneratorConfig] = new MemoryRepo[GeneratorConfig]
 
-  implicit val generatedMeta: Meta[GeneratedConfig] = Meta { value =>
-    value.config match {
-      case ID1(v) => ID(v)
-      case Instance(GeneratorConfig(id, _)) => ID(id)
-      case other => ???
-    }
-  }
-  val generated: MemoryRepo[GeneratedConfig] = new MemoryRepo[GeneratedConfig]
 
 
   "config generating config" in {
@@ -39,6 +30,61 @@ class GeneratorSpec extends WordSpec with Matchers {
     val generated = config.generate
     val values = generated.generate
     values.size should be (16384)
+  }
+
+  implicit val configsMeta: Meta[GeneratorConfig] = Meta(value => ID(value.id))
+  val configs: MemoryRepo[GeneratorConfig] = new MemoryRepo[GeneratorConfig]
+
+  implicit val generatedMeta: Meta[GeneratedConfig] = Meta { value =>
+    value.config match {
+      case ID1(v) => ID(v)
+      case Instance(GeneratorConfig(id, _)) => ID(id)
+      case other => ???
+    }
+  }
+  val generatedConfigs: MemoryRepo[GeneratedConfig] = new MemoryRepo[GeneratedConfig]
+
+  implicit val valuesMeta: Meta[GeneratedValues] = Meta { value =>
+    value.config match {
+      case ID1(v) => ID(v, value.id)
+      case Instance(GeneratorConfig(id, _)) => ID(id, value.id)
+      case other => ???
+    }
+  }
+  val generatedValues: MemoryRepo[GeneratedValues] = new MemoryRepo[GeneratedValues]
+
+  "save all to repos" in {
+    val config = GeneratorConfig(1, "first")
+    val generated = config.generate
+    val values = generated.generate
+
+    configs.create(config).attempt.unsafeRunSync() should be (Right(Created(ID(1), config)))
+    generatedConfigs.create(generated).attempt.unsafeRunSync() should be (Right(Created(ID(1), generated)))
+    generatedValues.createAll(values).attempt.unsafeRunSync()/* should be (
+      Right(AllCreated(values.groupBy(v => (v.config, v.id)).mapValues(_.head)))
+    )*/
+
+    configs(Read(ID(1))) should be (Result(ID(1), config))
+    generatedConfigs(Read(ID(1))) should be (Result(ID(1), generated))
+    generatedValues(Read(ID(1, 1))) should be (Result(ID(1, 1), values(1 - 1)))
+    generatedValues(Read(ID(1, 16384))) should be (Result(ID(1, 16384), values(16384 - 1)))
+  }
+
+  "endure 100 configs" in {
+    (2 to 100)
+      .map(i => GeneratorConfig(i, s"$i'th config"))
+      .map { config =>
+        configs(Create(config.identify, config))
+        config.generate
+      }.map { generated =>
+        generatedConfigs(Create(generated.identify, generated))
+        generated.generate
+      }.map { values =>
+        generatedValues(CreateAll(values.groupBy(v => (v.config, v.id)).mapValues(_.head)))
+    }
+    configs.count().unsafeRunSync() should be (100)
+    generatedConfigs.count().unsafeRunSync() should be (100)
+    generatedValues.count().unsafeRunSync() should be (16384 * 100) // 1.6 M+, 16 sec, 100K+/sec on my mac
   }
 
 }
